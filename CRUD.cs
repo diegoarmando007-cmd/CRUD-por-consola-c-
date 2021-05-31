75d3e6a14d25b69e9930f335e311f42e24450ac6
diego ortega <84762210+diegoarmando007-cmd@users.noreply.github.com>
2021-05-31 09:32:38 -0500

    Add files via upload

---
+++ CRUD.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace crud
+{
+    //CRUD son 4 palabras en ingles create, consultar, update  y delete
+    class CRUD
+    {
+        public void insertar() {
+            //LAB2\SQLEXPRESS
+            try
+            {
+
+                string conexion = @"data source = DESKTOP-BPIPVCL; initial catalog = curso_adonet;
+                           persist security info = true; Integrated Security = SSPI;";
+
+                string query = "insert into productos values(1,'sd',2,12)";
+
+                SqlConnection con = new SqlConnection(conexion);
+                con.Open();
+
+                SqlCommand cm = new SqlCommand(query, con);
+                cm.CommandText = query;
+
+                SqlDataReader dr = cm.ExecuteReader();
+
+                DataTable dt = new DataTable();
+                dt.Load(dr);
+
+                dr.Close();
+
+                Console.WriteLine("Registro insertado");
+
+                con.Close();
+
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("Error Insertar: " + ex.Message);
+            }
+            }
+
+        public void consultar()
+        {
+            try
+            {
+
+                string conexion = @"data source = DESKTOP-BPIPVCL; initial catalog = curso_adonet;
+                           persist security info = true; Integrated Security = SSPI;";
+
+                string query = "select * from productos";
+
+                SqlConnection con = new SqlConnection(conexion);
+                con.Open();
+
+                SqlCommand cm = new SqlCommand(query, con);
+                //ejecuta el comando text almacenado
+                cm.CommandText = query;
+
+                
+                SqlDataReader dr = cm.ExecuteReader();
+
+                //cargar la informacion del dr
+                DataTable dt = new DataTable();
+                dt.Load(dr);
+
+                dr.Close();
+
+                if (dt.Rows.Count > 0)
+                {
+                    for (int i = 0; i < dt.Rows.Count; i++)
+                    {
+                        Console.WriteLine("Matricula: " + dt.Rows[i]["folio"]);
+                        Console.WriteLine("Nomre: " + dt.Rows[i]["producto"]);
+                        Console.WriteLine("APP: " + dt.Rows[i]["cantidad"]);
+                        Console.WriteLine("APM: " + dt.Rows[i]["precio"]);
+                        //Console.WriteLine("EDAD: " + dt.Rows[i]["EDAD"]);
+                        Console.WriteLine("-------------");
+                    }
+                }
+                else {
+                    Console.WriteLine("No existe registros");
+                }
+
+                
+                con.Close();
+
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("Error Actualizar: " + ex.Message);
+            }
+        }
+
+        public void actualizar()
+        {
+            //LAB2\SQLEXPRESS
+            try
+            {
+
+                string conexion = @"data source = DESKTOP-BPIPVCL; initial catalog = curso_adonet;
+                           persist security info = true; Integrated Security = SSPI;";
+
+                string query = "update productos set producto = 'Prueba' where folio = 5";
+
+                SqlConnection con = new SqlConnection(conexion);
+                con.Open();
+
+                SqlCommand cm = new SqlCommand(query, con);
+                //ejecuta el comando text almacenado
+                cm.CommandText = query;
+
+
+                SqlDataReader dr = cm.ExecuteReader();
+
+                //cargar la informacion del dr
+                DataTable dt = new DataTable();
+                dt.Load(dr);
+
+                dr.Close();
+
+
+                Console.WriteLine("Registro actualizado");
+
+                dr.Close();
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("Error Actualizar: " + ex.Message);
+            }
+        }
+
+        public void Eliminar()
+        {
+            //LAB2\SQLEXPRESS
+            try
+            {
+
+                string conexion = @"data source = DESKTOP-BPIPVCL; initial catalog = curso_adonet;
+                           persist security info = true; Integrated Security = SSPI;";
+
+                string query = "delete from productos where folio = 1";
+
+                SqlConnection con = new SqlConnection(conexion);
+                con.Open();
+
+                SqlCommand cm = new SqlCommand(query, con);
+                //ejecuta el comando text almacenado
+                cm.CommandText = query;
+
+
+                SqlDataReader dr = cm.ExecuteReader();
+
+                //cargar la informacion del dr
+                DataTable dt = new DataTable();
+                dt.Load(dr);
+
+                dr.Close();
+
+
+                Console.WriteLine("Registro eliminado");
+
+                dr.Close();
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("Error de Eliminar: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ Principal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace crud
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  CRUD r = new CRUD();
+            //r.insertar();
+
+            CRUD e = new CRUD();
+            e.Eliminar();
+
+            //CRUD m = new CRUD();
+            //m.actualizar();
+
+           // CRUD t = new CRUD();
+            //t.consultar();
+
+            Console.Read();
+
+        }
+    }
+}
